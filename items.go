7b775f928716b5b0a9f228c886dc8a646886e31d
びくんびくん <big.stone.net@gmail.com>
2021-01-02 17:36:15 +0900

    itemを全件取得するように修正、Responseで返す型を変更

--- items.go
+++ items.go
@@ -21,12 +21,13 @@ type ItemResult struct {
 	Response       *ItemResponse `json:"Response,omitempty"`
 }
 
-func (c *Client) GetItems(t string) (*ItemResult, error) {
+func getItems(c *Client, t string, o int64, v *View) (*ItemResponse, error) {
 	rb := ItemRequest{
 		requestBase: c.requestBase,
-		Offset:      0,
-		View:        &View{},
+		Offset:      o,
+		View:        v,
 	}
+
 	s, err := json.Marshal(rb)
 	if err != nil {
 		return nil, err
@@ -57,18 +58,58 @@ func (c *Client) GetItems(t string) (*ItemResult, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &r, nil
 
+	return r.Response, nil
+}
+
+//もっとうまい実装方法は無いものだろうか・・・
+func (c *Client) GetItems(tableID string, filter *View) ([]ItemData, error) {
+	var r *ItemResponse
+	r, err := getItems(c, tableID, 0, filter)
+	if err != nil {
+		return nil, err
+	}
+	completed := false
+	itemData := make([]ItemData, 0, r.TotalCount)
+	encountered := map[int]bool{}
+	for _, d := range r.Data {
+		if !encountered[d.IssueID] {
+			encountered[d.IssueID] = true
+			itemData = append(itemData, d)
+		}
+	}
+
+	if r.TotalCount == len(itemData) {
+		completed = true
+	}
+
+	for completed == false {
+		r, err = getItems(c, tableID, int64(len(itemData)/r.PageSize), filter)
+		if err != nil {
+			return nil, err
+		}
+		for _, d := range r.Data {
+			if !encountered[d.IssueID] {
+				encountered[d.IssueID] = true
+				itemData = append(itemData, d)
+			}
+		}
+
+		if r.TotalCount == len(itemData) {
+			completed = true
+		}
+	}
+	return itemData, nil
 }
 
 type ItemResponse struct {
-	Offset     int     `json:"Offset,omitempty"`
-	PageSize   int     `json:"PageSize,omitempty"`
-	TotalCount int     `json:"TotalCount,omitempty"`
-	Data       []Datum `json:"Data,omitempty"`
+	Offset     int        `json:"Offset,omitempty"`
+	PageSize   int        `json:"PageSize,omitempty"`
+	TotalCount int        `json:"TotalCount,omitempty"`
+	Data       []ItemData `json:"Data,omitempty"`
 }
 
-type Datum struct {
+type ItemData struct {
 	APIVersion         float64          `json:"ApiVersion,omitempty"`
 	SiteID             int              `json:"SiteId,omitempty"`
 	UpdatedTime        string           `json:"UpdatedTime,omitempty"`
@@ -78,9 +119,9 @@ type Datum struct {
 	Body               string           `json:"Body,omitempty"`
 	StartTime          string           `json:"StartTime,omitempty"`
 	CompletionTime     string           `json:"CompletionTime,omitempty"`
-	WorkValue          int              `json:"WorkValue,omitempty"`
-	ProgressRate       int              `json:"ProgressRate,omitempty"`
-	RemainingWorkValue int              `json:"RemainingWorkValue,omitempty"`
+	WorkValue          float64          `json:"WorkValue,omitempty"`
+	ProgressRate       float64          `json:"ProgressRate,omitempty"`
+	RemainingWorkValue float64          `json:"RemainingWorkValue,omitempty"`
 	Status             int              `json:"Status,omitempty"`
 	Manager            int              `json:"Manager,omitempty"`
 	Owner              int              `json:"Owner,omitempty"`
